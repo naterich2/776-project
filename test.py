ebb72a087a1bca1c2460f78d17590f5baa2e7c39
Nate Richman <nate.rich2@gmail.com>
2020-04-23 13:13:59 -0500

    Normalize by sample and not by gene

--- test.py
+++ test.py
@@ -14,6 +14,8 @@ from scipy.cluster.hierarchy import dendrogram
 
 from sklearn.manifold import TSNE
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 
 import pydiffmap as dm
@@ -21,7 +23,7 @@ from pydiffmap.visualization import embedding_plot, data_plot
 
 
 ### Load datasets
-gse101521 = pd.read_csv('data/GSE101521_totalRNA_counts.csv',index_col=0).T
+gse101521 = pd.read_csv('data/GSE101521_totalRNA_counts.csv',index_col=0)
 gse101521_meta = pd.read_csv('data/GSE101521_samples_metadata.csv',index_col=0)
 gse101521_meta['diagnosis'] = gse101521_meta['diagnosis'].replace(r'CON','C',regex=True)
 gse101521_meta['diagnosis'] = gse101521_meta['diagnosis'].replace(r'MDD-S','M',regex=True)
@@ -29,27 +31,33 @@ gse101521_meta['diagnosis'] = gse101521_meta['diagnosis'].replace(r'MDD','M',reg
 gse101521_meta.index = gse101521_meta['name']
 
 # Find indices of control or MDD
-gse101521_c_ids = pd.Index(gse101521_meta[gse101521_meta['diagnosis'] == 'C']['name']).intersection(gse101521.index)
-gse101521_m_ids = pd.Index(gse101521_meta[gse101521_meta['diagnosis'] == 'M']['name']).intersection(gse101521.index)
+gse101521_c_ids = pd.Index(gse101521_meta[gse101521_meta['diagnosis'] == 'C']['name']).intersection(gse101521.columns)
+gse101521_m_ids = pd.Index(gse101521_meta[gse101521_meta['diagnosis'] == 'M']['name']).intersection(gse101521.columns)
 
 #GSE80655 dataset
-gse80655 = pd.read_csv('data/GSE80655_data_norm.csv',index_col=0).T
+gse80655 = pd.read_csv('data/GSE80655_data_norm.csv',index_col=0)
 gse80655_meta = pd.read_csv('data/GSE80655_meta.csv')
 
-gse80655 = gse80655.loc[gse80655_meta[gse80655_meta['brain_region'] == 'DLPFC'].index.intersection(gse80655.index),:]
+gse80655 = gse80655.loc[:,gse80655_meta[gse80655_meta['brain_region'] == 'DLPFC'].index.intersection(gse80655.columns)]
 
-gse80655_c_ids = gse80655_meta[gse80655_meta['diagnosis'] == 'C'].index.intersection(gse80655.index)
-gse80655_m_ids = gse80655_meta[gse80655_meta['diagnosis'] == 'M'].index.intersection(gse80655.index)
+gse80655_c_ids = gse80655_meta[gse80655_meta['diagnosis'] == 'C'].index.intersection(gse80655.columns)
+gse80655_m_ids = gse80655_meta[gse80655_meta['diagnosis'] == 'M'].index.intersection(gse80655.columns)
 
 ### Normalizatoin
 # Normalize
 ss = StandardScaler()
-gse80655 = pd.DataFrame(data=ss.fit_transform(gse80655),
+rs = RobustScaler()
+ms = MinMaxScaler()
+gse80655_scaled = ss.fit_transform(gse80655)
+gse80655 = pd.DataFrame(data=gse80655_scaled,
         index=gse80655.index,
         columns=gse80655.columns)
-gse101521 = pd.DataFrame(data=ss.fit_transform(gse101521),
+gse80655 = gse80655.T
+gse101521_scaled = ss.fit_transform(gse101521)
+gse101521 = pd.DataFrame(data=gse101521_scaled,
         index=gse101521.index,
         columns=gse101521.columns)
+gse101521 = gse101521.T
 gse80655_age_info = gse80655_meta[((gse80655_meta['diagnosis'] == 'C') | (gse80655_meta['diagnosis'] == 'M')) & (gse80655_meta['brain_region'] == 'DLPFC')].loc[:,('age','diagnosis')].reindex(index=gse80655.index)
 gse101521_age_info = gse101521_meta[(gse101521_meta['diagnosis'] == 'C') | (gse101521_meta['diagnosis'] == 'M')].loc[:,('age','diagnosis')].reindex(index=gse101521.index)
 age_info = gse80655_age_info.append(gse101521_age_info)
