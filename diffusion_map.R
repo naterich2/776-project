f28d5ad69cc07b9ce3ad5f40cb027fa1b162b842
Nate Richman <nate.rich2@gmail.com>
2020-05-09 20:30:19 -0500

    Added running diffusionmap, tsne, and pca embeddings for genes associated with depression from brainspan

--- diffusion_map.R
+++ diffusion_map.R
@@ -2,14 +2,22 @@ library('destiny')
 
 setwd("~/project")
 
-all_sig = read.csv('data/p_all_sig.csv')
+all_sig <- read.csv('data/p_all_sig.csv')
+diag_sig <- read.csv('data/p_diag_sig.csv')
+depr_sig <- read.csv('data/p_depr.csv')
 
 
 diffmap_all <- DiffusionMap(data = all_sig)
+diffmap_diag <- DiffusionMap(data = diag_sig)
+diffmap_depr <- DiffusionMap(data = depr_sig)
 
-eigs_all <- as.data.frame(eigenvectors(diffmap_all)
+eigs_all <- as.data.frame(eigenvectors(diffmap_all))
+eigs_diag <- as.data.frame(eigenvectors(diffmap_diag))
+eigs_depr <- as.data.frame(eigenvectors(diffmap_depr))
 
 
 write.csv(eigs_all,file='data/p_all_sig_dm.csv')
+write.csv(eigs_diag,file='data/p_diag_sig_dm.csv')
+write.csv(eigs_depr,file='data/p_depr_dm.csv')
 
 
--- diffusion_map.py
+++ diffusion_map.py
@@ -4,9 +4,17 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 from matplotlib.lines import Line2D
 import subprocess
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+from plot_embeddings import plot_embedding
+
+plt.switch_backend('Agg')
 # Import total data
 data = pd.read_csv('data/total_data.csv',index_col=0)
 meta = pd.read_csv('data/total_meta.csv',index_col=0)
+depr = pd.read_csv('data/depr_row.csv',index_col=0)
 
 # Import p-values from permutations
 p_all = pd.read_csv('data/p_vals_all.csv',index_col=0)
@@ -18,36 +26,59 @@ p_all_sig = data.loc[:,p_all_sig_genes.index]
 p_all_sig.to_csv('data/p_all_sig.csv',index_label=False)
 
 # Select significant genes from diagnosis+expression MI and export
+p_diag_sig_genes = p_diag[p_diag['p'] <= p_diag['bounds']]
+p_diag_sig = data.loc[:,p_diag_sig_genes.index]
+p_diag_sig.to_csv('data/p_diag_sig.csv',index_label=False)
 
+#
+p_depr = data.loc[:,depr['name']]
+p_depr.to_csv('data/p_depr.csv')
 # Run Rscript
 rscript = subprocess.Popen(['Rscript','diffusion_map.R'])
 rscript.wait()
 
 # Import data
-p_all_dm = pd.read_csv('data/p_all_sig_dm.csv')
-p_diag_dm = pd.read_csv('data/p_diag_sig_dm.csv')
+p_all_dm = pd.read_csv('data/p_all_sig_dm.csv',index_col=0)
+p_diag_dm = pd.read_csv('data/p_diag_sig_dm.csv',index_col=0)
+p_depr_dm = pd.read_csv('data/p_depr_dm.csv',index_col=0)
 
 ## Plot data
-fig,[ax1,ax2] = plt.subplots(1,2,figsize=(16,8),sharey=True)
-ages = meta['age']
-diagnosis = meta['diagnosis'].str.lower()
-age_scatter = ax1.scatter(p_all_dm.iloc[:,0],p_all_dm.iloc[:,1],
-        c=ages,
-        norm=Normalize(np.min(ages),np.max(ages)),
-        cmap='spring')
-ax1.set_ylabel('DC2')
-ax1.set_xlabel('DC1')
-ax1.set_title('Diffusion map embedding vs. age')
-fig.colorbar(age_scatter,ax=ax1)
-ax2.scatter(p_all_dm.iloc[:,0],p_all_dm.iloc[:,1],
-    c=diagnosis)
-control=Line2D([0],[0],color='c',marker='o',markerfacecolor='c',
-        markersize=6,lw=0,label='Control')
-mdd=Line2D([0],[0],lw=0,color='m',marker='o',markerfacecolor='m',
-        markersize=6,label='MDD')
-ax2.legend(handles=[control,mdd])
-ax2.set_xlabel('DC1')
-ax2.set_title('Diffusion map embedding vs. diagnosis')
+tsne_diag = TSNE(n_components=3)
+tsne_all = TSNE(n_components=3)
+tsne_depr = TSNE(n_components=3)
+
+pca_diag = PCA(n_components=3)
+pca_all = PCA(n_components=3)
+pca_depr = PCA(n_components=3)
+
+tsne_diag_points = tsne_diag.fit_transform(p_diag_sig)
+tsne_all_points = tsne_all.fit_transform(p_all_sig)
+tsne_depr = tsne_all.fit_transform(p_depr_dm)
+
+plot_embedding(tsne_diag_points,meta,'TSNE embeddings (expression,diagnosis)',
+        outfile='figs/tsne_diag.png',labels='TSNE')
+plot_embedding(tsne_all_points,meta,'TSNE embeddings (expression,age,diagnosis)',
+        outfile='figs/tsne_all.png',labels='TSNE')
+plot_embedding(tsne_depr,meta,'TSNE embeddings depression genes',
+        outfile='figs/tsne_depr.png',labels='TSNE')
+
+pca_diag_points = pca_diag.fit_transform(p_diag_sig)
+pca_all_points = pca_all.fit_transform(p_all_sig)
+
+plot_embedding(pca_diag_points,meta,'PCA scores (expression,diagnosis)',
+        outfile='figs/pca_diag.png',labels='PC')
+plot_embedding(pca_all_points,meta,'PCA scores (expression,age,diagnosis)',
+        outfile='figs/pca_all.png',labels='PC')
+plot_embedding(pca_depr,meta,'PCA scores depression genes',
+        outfile='figs/pca_depr.png',labels='PC')
+
+plot_embedding(p_diag_dm.values,meta,'Diffusionmap embeddings (expression,diagnosis)',
+        outfile='figs/dm_diag.png',labels='DC')
+plot_embedding(p_all_dm.values,meta,'Diffusionmap embeddings (expression,age,diagnosis)',
+        outfile='figs/dm_all.png',labels='DC')
+plot_embedding(p_depr.values,meta,'Diffusionmap embeddings depression genes',
+        outfile='figs/dm_all.png',labels='DC')
+
 
 
 
